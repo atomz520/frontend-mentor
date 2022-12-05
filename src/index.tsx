4cbe922628353153529ec817fb86bc9a136e954b
Shalom Quek <shalomquek@gmail.com>
2022-12-05 22:37:12 +0800

    Installed Chakra

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react'
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home';
+import InteractiveRatingComponent from './challenges/InteractiveRatingComponent';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/interactive-rating-component",
+    element: <InteractiveRatingComponent />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ChakraProvider>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+      {/* <App /> */}
+    </React.StrictMode>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container } from '@chakra-ui/react'
+
+const Home = () => {
+  return (
+    <Container>
+      <div>Home</div>
+      There are many benefits to a joint design and development system. Not only
+      does it bring benefits to the design team, but it also brings benefits to
+      engineering teams. It makes sure that our experiences have a consistent look
+      and feel, not just in our design specs, but in production
+    </Container>
+    
+  )
+}
+
+export default Home
