2ad1ccb7136ae2ac5d93d28a3b3546fad179aab5
Shalom Quek <shalomquek@gmail.com>
2022-12-13 21:05:37 +0800

    added new challenge

---
+++ src/challenges/skilled-elearning-landing-page/SkilledElearningLandingPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SkilledElearningLandingPage = () => {
+  return (
+    <div>SkilledElearningLandingPage</div>
+  )
+}
+
+export default SkilledElearningLandingPage
---
+++ src/challenges/skilled-elearning-landing-page/theme.ts
@@ -0,0 +1,10 @@
+export default {
+  colors: {
+    purple: '#666CA3',
+    darkPurple: '#13183F',
+    grey: '#83869A',
+    pink: '#F74780',
+    lightPink: '#FFA7C3',
+    white: '#FFFFFF',
+  },
+}
