6286dce12bd0ac8dfbb87374180f71923f1214f6
Shalom Quek <shalomquek@gmail.com>
2022-12-14 00:49:14 +0800

    Added List of challenges to homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import { Container } from '@chakra-ui/react'
+import { Container, Heading } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <Container>
-      <div>Home</div>
-      There are many benefits to a joint design and development system. Not only
-      does it bring benefits to the design team, but it also brings benefits to
-      engineering teams. It makes sure that our experiences have a consistent look
-      and feel, not just in our design specs, but in production
+    <Container maxW='container.lg'>
+      <Heading>Frontend Mentor Challenges</Heading>
+      <Link to='/interactive-rating-component'>Interactive Rating Component</Link>
     </Container>
     
   )
