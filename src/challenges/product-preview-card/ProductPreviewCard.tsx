0685b72179542fbe40ce29165152cc8cc7ca4a7f
Shalom Quek <shalomquek@gmail.com>
2023-02-19 00:55:46 +0800

    Changed to CSS modules to prevent CSS conflict

--- src/challenges/product-preview-card/ProductPreviewCard.tsx
+++ src/challenges/product-preview-card/ProductPreviewCard.tsx
@@ -1,26 +1,28 @@
 import React from 'react'
-import "./style.css"
+import ProductPreviewCardCSS from "./ProductPreviewCard.module.css"
 import cartIcon from "./icon-cart.svg"
 
 export const ProductPreviewCard = () => {
   return (
-    <div className="container">
-      <div className="card-left">
-      </div>
-      <div className="card-right">
-        <p className="product-category">PERFUME</p>
-        <h1>Gabrielle Essence Eau De Parfum</h1>
-        <p>
-          A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.
-        </p>
-        <div className="pricing">
-          <h1 className="price">$149.99</h1>
-          <p className="original-price">$169.99</p>
+    <div className={ProductPreviewCardCSS.body}>
+      <div className={ProductPreviewCardCSS.container}>
+        <div className={ProductPreviewCardCSS.cardLeft}>
         </div>
-        <button className="add-cart-btn">
-          <img src={cartIcon}/>
-          Add to Cart
-        </button>
+        <div className={ProductPreviewCardCSS.cardRight}>
+          <p className={ProductPreviewCardCSS.productCategory}>PERFUME</p>
+          <h1 className={ProductPreviewCardCSS.heading + ' ' + ProductPreviewCardCSS.h1}>Gabrielle Essence Eau De Parfum</h1>
+          <p className={ProductPreviewCardCSS.paragraph}>
+            A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.
+          </p>
+          <div className={ProductPreviewCardCSS.pricing}>
+            <h1 className={ProductPreviewCardCSS.heading + ' ' + ProductPreviewCardCSS.h1 + ' ' + ProductPreviewCardCSS.price}>$149.99</h1>
+            <p className={ProductPreviewCardCSS.originalPrice}>$169.99</p>
+          </div>
+          <button className={ProductPreviewCardCSS.addCartBtn}>
+            <img src={cartIcon}/>
+            Add to Cart
+          </button>
+        </div>
       </div>
     </div>
   )
